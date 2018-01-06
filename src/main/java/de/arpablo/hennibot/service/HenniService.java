2a28b5e2b47f4a1e48dfcc676db5eed5a4181814
arpablo <armin.pfarr@gmail.com>
2018-01-06 09:52:02 +0100

    Added initaila message handler

--- src/main/java/de/arpablo/hennibot/service/HenniService.java
+++ src/main/java/de/arpablo/hennibot/service/HenniService.java
@@ -3,8 +3,16 @@
  */
 package de.arpablo.hennibot.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.exception.MessengerApiException;
+import com.github.messenger4j.exception.MessengerIOException;
+import com.github.messenger4j.send.MessagePayload;
+import com.github.messenger4j.send.message.TextMessage;
 import com.github.messenger4j.webhook.event.AttachmentMessageEvent;
 import com.github.messenger4j.webhook.event.TextMessageEvent;
 
@@ -15,10 +23,24 @@ import com.github.messenger4j.webhook.event.TextMessageEvent;
 @Service
 public class HenniService {
 
-		public void processTextMessage(TextMessageEvent event) {
-		}
-		
-		public void processAttachmentMessage(AttachmentMessageEvent event) {
-		}
 	
+	private static final Logger log = LoggerFactory.getLogger(HenniService.class);
+
+	@Autowired
+	private Messenger messenger;
+
+	public void processTextMessage(TextMessageEvent event) {
+		String senderId = event.senderId();
+		final TextMessage textMessage = TextMessage.create("I got a text message");
+        final MessagePayload messagePayload = MessagePayload.create(senderId, textMessage);
+		try {
+			messenger.send(messagePayload);
+		} catch (MessengerApiException | MessengerIOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	public void processAttachmentMessage(AttachmentMessageEvent event) {
+	}
+
 }
