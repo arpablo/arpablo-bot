ddbece8c8f0b9edc9a804139ca3c13778f74f7b1
arpablo <armin.pfarr@gmail.com>
2018-01-06 10:39:39 +0100

    Updated handleRequest

--- src/main/java/de/arpablo/hennibot/controller/MessengerCallbackController.java
+++ src/main/java/de/arpablo/hennibot/controller/MessengerCallbackController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +52,7 @@ public class MessengerCallbackController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> handleMessage(String requestPayload, Optional<String> signature) {
+	public ResponseEntity<Void> handleMessage(@RequestBody String requestPayload, Optional<String> signature) {
 		try {
 			messenger.onReceiveEvents(requestPayload, signature, event -> {
 				if (event.isTextMessageEvent()) {
